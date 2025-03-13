d94b5a770b5e2a2366ddd6cb63c8a1e5aaf587d8
Clément Rea <clement.rea@aidalinfo.fr>
2025-03-13 16:31:40 +0100

    ✨ FEAT: create all navbar pages in front

---
+++ Frontend/src/layouts/MainHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container bg-light10 flex column justify-center items-center rounded-borders-bottom">
+    <q-img class="logo" :src="logo" alt="logo" />
+    <div class="span_container flex justify-end column items-start q-pl-lg q-mb-xl">
+      <span class="text-dark80 text-bold text-subtitle1">L'application de quiz ludique</span>
+      <span class="text-dark80 text-bold text-subtitle2">Pour vous défier entre amis !</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import logo from '../assets/logo_quiz_fiesta.svg'
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  z-index: 99;
+  border-radius: 0px 0px 20px 20px;
+}
+
+.logo {
+  width: 200px;
+}
+
+.span_container {
+  width: 100%;
+}
+</style>
---
+++ Frontend/src/pages/Scores/ScoresIndexPage.vue
@@ -0,0 +1,1 @@
+<template>Scores page</template>
