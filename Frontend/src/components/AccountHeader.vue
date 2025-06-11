b0386f7d0bcb1f69ba837e53ba33fc18d6f5cd25
Clément Rea <clement.rea@aidalinfo.fr>
2025-06-11 14:37:10 +0200

    ✨ Feat: Add BackArrow component and integrate it into AccountHeader and FormLayout for improved navigation

--- Frontend/src/components/AccountHeader.vue
+++ Frontend/src/components/AccountHeader.vue
@@ -1,16 +1,16 @@
 <template>
-  <div
-    class="row flex items-center justify-between full-width q-pa-md"
-    style="margin-bottom: 116px"
-  >
-    <span class="text-h4 text-dark90 text-bold">Profil</span>
-    <!-- TODO : rediriger vers la page de parametres et faire cette page là -->
+  <div class="row items-center justify-between full-width q-pa-md" style="margin-bottom: 116px">
+    <div class="row items-center">
+      <BackArrow v-if="$route.meta.showBackArrow" />
+      <span class="text-h4 text-dark90 text-bold q-ma-none">Profil</span>
+    </div>
     <q-icon name="mdi-cog" color="dark90" size="lg" @click="router.push('/settings')" />
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
+import BackArrow from 'src/components/BackArrow.vue'
 
 const router = useRouter()
 </script>
